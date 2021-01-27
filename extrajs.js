8382e6ba3603da88adc6a4f42fcddb24581a0a1c
jhambach <64616275+jhambach@users.noreply.github.com>
2021-01-27 17:09:44 -0600

    Update extrajs.js

--- extrajs.js
+++ extrajs.js
@@ -1,12 +1,6 @@
-// store the toolbar element for reference
-var appmenu = document.getElementsByClassName("dropdown-menu"); 
-
-// Create LI wrapper 
-var appmenuitem = document.createElement("LI"); 
-
-// Set the innerHTML of the LI wrapper to our link
-// The <span ...</span> is just to add a "Beta" icon
-appmenuitem.innerHTML = '<a href="https://support.snapcom.com" target="_blank">Support<span style="background-color: #d80000; color: #fff; margin-left: 2px; border-radius: 6px; font-size: .7em; padding: 3px 4px; vertical-align: super;">BETA</span></a>';
-
-// Insert the LI element before the first existing item.
-appmenu.insertAfter(appmenu,appmenuitem);
+<script>
+  window.announcekit = (window.announcekit || { queue: [], on: function(n, x) { window.announcekit.queue.push([n, x]); }, push: function(x) { window.announcekit.queue.push(x); } });
+  window.announcekit.push({
+    "widget": "https://announcekit.co/widgets/v2/26D3t6"
+  })
+</script>
