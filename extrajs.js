9dfc478c3f308d499b1f3977082a2883a6c623cc
jhambach <64616275+jhambach@users.noreply.github.com>
2021-01-27 17:17:43 -0600

    Update extrajs.js

--- extrajs.js
+++ extrajs.js
@@ -1,6 +1,4 @@
-<script>
-  window.announcekit = (window.announcekit || { queue: [], on: function(n, x) { window.announcekit.queue.push([n, x]); }, push: function(x) { window.announcekit.queue.push(x); } });
-  window.announcekit.push({
+window.announcekit = (window.announcekit || { queue: [], on: function(n, x) { window.announcekit.queue.push([n, x]); }, push: function(x) { window.announcekit.queue.push(x); } });
+window.announcekit.push({
     "widget": "https://announcekit.co/widgets/v2/26D3t6"
   })
-</script>
