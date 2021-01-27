ce9c5a6e521bd67be39d4ade1c6094c01df5f1a4
jhambach <64616275+jhambach@users.noreply.github.com>
2021-01-27 17:06:47 -0600

    Update extrajsconfiguration.js

--- extrajsconfiguration.js
+++ extrajsconfiguration.js
@@ -1,5 +1,5 @@
 window.announcekit = (window.announcekit || { queue: [], on: function(n, x) { window.announcekit.queue.push([n, x]); }, push: function(x) { window.announcekit.queue.push(x); } });
-  window.announcekit.push({
+ window.announcekit.push({
     "widget": "https://announcekit.co/widgets/v2/26D3t6"
   })
 src="https://cdn.announcekit.app/widget-v2.js"
