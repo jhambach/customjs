ac0382b5a28241afc4c5e9abeb33875fbe9b46de
jhambach <64616275+jhambach@users.noreply.github.com>
2021-01-27 17:14:25 -0600

    Create reference.js

---
+++ reference.js
@@ -0,0 +1,6 @@
+var my_awesome_script1 = document.createElement('script');
+var my_awesome_script2 = document.createElement('script');
+my_awesome_script1.setAttribute('src','https://raw.githubusercontent.com/jhambach/customjs/main/extrajs.js');
+my_awesome_script2.setAttribute('src','https://raw.githubusercontent.com/jhambach/customjs/main/extrajsconfiguration.js');
+document.head.appendChild(my_awesome_script1);
+document.head.appendChild(my_awesome_script2);
